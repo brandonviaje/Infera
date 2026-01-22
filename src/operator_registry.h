99d6ddfa8e898782cf4610b92eaaa9b4b4b1b81a
brandonviaje <viajebrandon@gmail.com>
2026-01-22 14:23:27 -0500

    refactor: reimplement operator registry

---
+++ src/operator_registry.h
@@ -0,0 +1,42 @@
+#ifndef REGISTRY_H
+#define REGISTRY_H
+
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <iostream>
+
+#include "operator.h"
+#include "ops/flatten.h"
+#include "ops/gemm.h"
+#include "ops/relu.h"
+#include "ops/add.h"
+
+class Registry
+{
+public:
+    //  simple factory design pattern
+    static std::unique_ptr<Operator> create(const std::string& type)
+    {
+        if(type == "Add")
+        {
+            return std::make_unique<AddOperator>();
+        }
+        if (type == "Flatten") 
+        {
+            return std::make_unique<FlattenOperator>();
+        }
+        else if (type == "Gemm")
+        {
+            return std::make_unique<GemmOperator>();
+        }
+        else if (type == "Relu")
+        {
+            return std::make_unique<ReluOperator>();
+        }
+        std::cerr << "Warning: Operator '" << type << "' not implemented yet." << std::endl; // else operator isn't registered/supported yet
+        return nullptr;
+    }
+};
+
+#endif
