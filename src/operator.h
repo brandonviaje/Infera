719d58762eb9396a54ee8d20929282d833ac3309
brandonviaje <viajebrandon@gmail.com>
2026-02-16 17:12:22 -0500

    feat: shape inference

--- src/operator.h
+++ src/operator.h
@@ -9,9 +9,10 @@
 class Operator 
 {
 public:
-    virtual ~Operator() = default;                                                                                // virtual destructor
-    virtual void set_attributes(const Node& node) { (void)node; }                                                 // load settings
-    virtual void forward(const std::vector<Tensor<float>*>& inputs, std::vector<Tensor<float>*>& outputs) = 0; // execute operator
+    virtual ~Operator() = default;                                                                                               // virtual destructor
+    virtual void set_attributes(const Node& node) = 0;                                                                           // load settings
+    virtual void compute_output_shapes(const std::vector<Tensor<float>*>& inputs, std::vector<Tensor<float>*>& outputs) = 0;     // shape inference
+    virtual void forward(const std::vector<Tensor<float>*>& inputs, std::vector<Tensor<float>*>& outputs) = 0;                   // execute operator
 };
 
 #endif
--- src/operator_registry.h
+++ src/operator_registry.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <memory>
-#include <stdexcept>
 #include <iostream>
 
 #include "operator.h"
@@ -11,30 +10,24 @@
 #include "ops/gemm.h"
 #include "ops/relu.h"
 #include "ops/add.h"
+#include "ops/conv.h"
+#include "ops/maxpool.h"
+#include "ops/reshape.h"
 
 class OperatorRegistry
 {
 public:
-    //  simple factory design pattern
+    // simple factory design pattern
     static std::unique_ptr<Operator> create_operator(const std::string& type)
     {
-        if(type == "Add")
-        {
-            return std::make_unique<AddOperator>();
-        }
-        if (type == "Flatten") 
-        {
-            return std::make_unique<FlattenOperator>();
-        }
-        else if (type == "Gemm")
-        {
-            return std::make_unique<GemmOperator>();
-        }
-        else if (type == "Relu")
-        {
-            return std::make_unique<ReluOperator>();
-        }
-        std::cerr << "Warning: Operator '" << type << "' not implemented yet." << std::endl; // else operator isn't registered/supported yet
+        if (type == "Add")          return std::make_unique<AddOperator>();
+        if (type == "Flatten")      return std::make_unique<FlattenOperator>();
+        if (type == "MaxPool")      return std::make_unique<MaxPoolOperator>();
+        if (type == "Conv")         return std::make_unique<ConvOperator>();
+        if (type == "Reshape")      return std::make_unique<ReshapeOperator>();
+        if (type == "Gemm")         return std::make_unique<GemmOperator>();
+        if (type == "Relu")         return std::make_unique<ReluOperator>();
+        std::cerr << "Warning: Operator '" << type << "' not implemented yet." << std::endl;
         return nullptr;
     }
 };
