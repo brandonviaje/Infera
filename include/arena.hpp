bb28f3a313bae262312757e3e7bd2d440b686409
brandonviaje <viajebrandon@gmail.com>
2026-04-14 22:23:19 -0400

    feat: cuda arena

--- include/arena.hpp
+++ include/arena.hpp
@@ -37,4 +37,38 @@ private:
   size_t offset;
 };
 
+class CudaArena {
+private:
+  float *d_base_ptr; 
+  size_t total_size; 
+  size_t offset;    
+
+public:
+  /**
+   * Create GPU memory arena.
+   *
+   * @param size_bytes Total VRAM pool size to allocate
+   */
+  CudaArena(size_t size_bytes);
+
+  ~CudaArena();
+
+  CudaArena(const CudaArena &) = delete;
+  CudaArena &operator=(const CudaArena &) = delete;
+
+  /**
+   * Allocates a chunk of GPU memory from the arena.
+   *
+   * @param bytes Number of bytes to allocate in device memory.
+   * @return Device pointer offset from the base allocation.
+   *
+   */
+  float *allocate(size_t bytes);
+
+  /**
+   * Resets GPU arena
+   * Does not free memory; only resets internal offset.
+   */
+  void reset();
+};
 #endif
--- src/arena.cpp
+++ src/arena.cpp
@@ -1,9 +1,9 @@
 #include "arena.hpp"
+#include <cuda_runtime.h>
 #include <iostream>
+#include <stdexcept>
 
-#ifdef _WIN32
-#include <malloc.h>
-#endif
+// tensor arena impl
 
 TensorArena::TensorArena(size_t size_in_bytes)
     : capacity(size_in_bytes), offset(0) {
@@ -17,20 +17,14 @@ TensorArena::TensorArena(size_t size_in_bytes)
     throw std::bad_alloc();
 }
 
-TensorArena::~TensorArena() {
-#ifdef _WIN32
-  _aligned_free(memory);
-#else
-  std::free(memory);
-#endif
-}
+TensorArena::~TensorArena() { std::free(memory); }
 
 void *TensorArena::allocate(size_t bytes, size_t alignment) {
   uintptr_t current_ptr = reinterpret_cast<uintptr_t>(memory + offset);
   size_t padding = (alignment - (current_ptr % alignment)) % alignment;
 
   if (offset + padding + bytes > capacity) {
-    return nullptr; // Out of memory
+    return nullptr; // out of memory
   }
 
   offset += padding;
@@ -43,3 +37,28 @@ void TensorArena::reset() { offset = 0; }
 
 size_t TensorArena::get_usage() const { return offset; }
 size_t TensorArena::get_capacity() const { return capacity; }
+
+// CUDA arena impl
+
+CudaArena::CudaArena(size_t size_bytes) : total_size(size_bytes), offset(0) {
+  cudaError_t err = cudaMalloc((void **)&d_base_ptr, total_size);
+  if (err != cudaSuccess) {
+    throw std::runtime_error("CudaArena failed to allocate VRAM");
+  }
+}
+
+CudaArena::~CudaArena() { cudaFree(d_base_ptr); }
+
+float *CudaArena::allocate(size_t bytes) {
+  size_t aligned_bytes = (bytes + 255) & ~255;
+
+  if (offset + aligned_bytes > total_size) {
+    throw std::bad_alloc(); // out of VRAM
+  }
+
+  float *ptr = d_base_ptr + (offset / sizeof(float));
+  offset += aligned_bytes;
+  return ptr;
+}
+
+void CudaArena::reset() { offset = 0; }
